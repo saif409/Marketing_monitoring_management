525fa08ec79b67ca33cf768073193487ba01b711
Nazmul Islam <md.nazmul@northsouth.edu>
2021-03-10 13:16:15 +0600

    fix close modal issue

--- sadmin/static/js/custom-script.js
+++ sadmin/static/js/custom-script.js
@@ -20,7 +20,7 @@ img2.onclick = function(){
 }
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+var span = document.getElementsByClassName("close-button")[0];
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
